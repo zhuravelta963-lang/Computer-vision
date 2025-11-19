9d982c34296151d0555a9c72f5e7c21759a5a947
zhuravelta963-lang <zhuravelta963@gmail.com>
2025-11-19 23:57:29 +0200

    Add files via upload

---
+++ data/trackbars.py
@@ -0,0 +1,6 @@
+[InternetShortcut]
+URL=https://github.com/Ivan-Komarov/computer-Vision/blob/main/data/trackbars.py
+IDList=
+HotKey=0
+IconFile=C:\Users\zhura\AppData\Local\Mozilla\Firefox\Profiles\wq6nczh6.default-release\shortcutCache\glEdH7XoBJ_H0ePaLrIGDWQ3m3kpDiQ1lwaH5pGXQfY=.ico
+IconIndex=0
